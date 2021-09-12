1872bae91a25d79d1b14f274281756da65d65cce
BA_CalderonMorales <tbcm640@gmail.com>
2021-09-11 19:53:18 -0500

    Created blank canvas of what the amazon clone will follow.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDglZ45CqQ-4qGStV1Mta9uk28Qq1A1dwQ",
+    authDomain: "challenge-156a7.firebaseapp.com",
+    projectId: "challenge-156a7",
+    storageBucket: "challenge-156a7.appspot.com",
+    messagingSenderId: "941797390840",
+    appId: "1:941797390840:web:f75f719674492d618cbe0d",
+    measurementId: "G-JEG0GG8NLW"
+};
